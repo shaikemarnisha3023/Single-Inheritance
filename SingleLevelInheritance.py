1f52b14ae62b5dc6d7e9c4ecc35a3440121df6a9
shaikemarnisha3023 <shaikemarnisha@gmail.com>
2025-09-20 10:05:32 +0530

    Add files via upload

---
+++ SingleLevelInheritance.py
@@ -0,0 +1,15 @@
+class alekhya():
+          def __init__(self,name,age,salary):
+                    self.name=name
+                    self.age=age
+                    self.salary=salary
+class marnisha(alekhya):
+          def __init__(self,name,age,salary,id):
+                    self.name=name
+                    self.age=age
+                    self.salary=salary
+                    self.id=id
+ale=alekhya("shaike Alekhya",35,500000)
+print(ale.age)
+                    
+                    
